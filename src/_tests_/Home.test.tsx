f78d4fcf87ecb6d8f2e494426f85f57e36a30912
shogo4131 <shougo80697118@gmail.com>
2021-10-17 14:25:18 +0900

    first commit

---
+++ src/_tests_/Home.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from '../pages/index';
+
+it('Should render hello text', () => {
+  render(<Home />);
+  expect(screen.getByText('Hello Nextjs')).toBeInTheDocument();
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
+      Hello Nextjs
+    </div>
+  );
+};
+
+export default Home;
